1dff3eadd7f013de8a3e00682c90983a31b02c3d
shawn0rion <shawnorion07@gmail.com>
2023-05-22 21:02:30 -0500

    search returns wiki results with hyperlink on click

---
+++ script.js
@@ -0,0 +1,51 @@
+// search events
+const searchInput = document.querySelector('#search-input');
+const searchBtn = document.querySelector('.search-btn');
+searchInput.addEventListener('keydown', async event => {
+    if (event.key === 'Enter' && event.target.value !== ''){
+        try{
+            const searchResults = await getSearchResults(event.target.value);
+            displayResults(searchResults); 
+
+        }
+        catch(error){
+            console.log(error);
+        };
+    }
+});
+
+async function getSearchResults(searchTerm){
+    try{
+        const response = await fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${searchTerm}&limit=10&namespace=0&format=json`)
+        const data = await response.json()
+        // reformat data
+        const numResults = data[1].length;
+        const arrayOfObjects = [];
+        for (let i = 0; i < numResults; i++){
+            arrayOfObjects.push({
+                title: data[1][i],
+                link: data[3][i],
+            })
+        }
+        console.log(arrayOfObjects);
+        return arrayOfObjects;
+
+    } catch(error){
+        console.log(error);
+    }
+}
+
+function displayResults(searchResults){
+    const listOfResults = document.querySelector('.search-results');
+    console.log(searchResults)
+    searchResults.forEach(searchResult => {
+        console.log(searchResult)
+        const result = document.createElement('li');
+        result.classList.add('search-result');
+        result.textContent = searchResult.title;
+        result.addEventListener('click', event => {
+            window.location.href = searchResult.link;
+        })
+        listOfResults.appendChild(result);
+    })
+}
